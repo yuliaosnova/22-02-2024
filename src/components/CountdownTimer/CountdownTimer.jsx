60763fc4034ea1bc0d96a50bdf9fd6850de8b934
Yulia Osnova <yulia.osnova@gmail.com>
2024-02-25 21:49:00 +0200

    FIX: fixed countown timer

--- src/components/CountdownTimer/CountdownTimer.jsx
+++ src/components/CountdownTimer/CountdownTimer.jsx
@@ -9,50 +9,34 @@ const CountdownTimer = () => {
   const selectedTripId = useSelector(getSelectedTripId);
   const selectedTrip = trips.find((trip) => trip.id === selectedTripId);
   const startDate = selectedTrip.startDate;
-  const timeLeft = calculateTimeLeft(startDate);
 
-  const [days, setDays] = useState(timeLeft.days);
-  const [hours, setHours] = useState(timeLeft.hours);
-  const [minutes, setMinutes] = useState(timeLeft.minutes);
-  const [seconds, setSeconds] = useState(timeLeft.seconds);
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(startDate));
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (seconds > 0) {
-        setSeconds((seconds) => seconds - 1);
-      } else if (minutes > 0) {
-        setMinutes((minutes) => minutes - 1);
-        setSeconds(59);
-      } else if (hours > 0) {
-        setHours((hours) => hours - 1);
-        setMinutes(59);
-        setSeconds(59);
-      } else if (days > 0) {
-        setDays((days) => days - 1);
-        setHours(23);
-        setMinutes(59);
-        setSeconds(59);
-      }
+      setTimeLeft(calculateTimeLeft(startDate));
     }, 1000);
     return () => clearInterval(interval);
-  }, [seconds, minutes, hours, days]);
+  }, [startDate]);
+
+  const { days, hours, minutes, seconds } = timeLeft;
 
   return (
     <div className={css.timer}>
       <div className={css.timer_item}>
-        <p className={css.time_value}>{timeLeft.days}</p>
+        <p className={css.time_value}>{days}</p>
         <p className={css.time_label}>Days</p>
       </div>
       <div className={css.timer_item}>
-        <p className={css.time_value}>{timeLeft.hours}</p>
+        <p className={css.time_value}>{hours}</p>
         <p className={css.time_label}>Hours</p>
       </div>
       <div className={css.timer_item}>
-        <p className={css.time_value}>{timeLeft.minutes}</p>
+        <p className={css.time_value}>{minutes}</p>
         <p className={css.time_label}>Minutes</p>
       </div>
       <div className={css.timer_item}>
-        <p className={css.time_value}>{timeLeft.seconds}</p>
+        <p className={css.time_value}>{seconds}</p>
         <p className={css.time_label}>Seconds</p>
       </div>
     </div>
@@ -60,3 +44,4 @@ const CountdownTimer = () => {
 };
 
 export default CountdownTimer;
+
